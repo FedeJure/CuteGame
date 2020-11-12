d8f244d707665323180775275e89866630b6e39b
Federico Jure <fjure@atixlabs.com>
2020-11-12 00:04:58 -0300

    improve ui

---
+++ Assets/Models/Donut/UIDonut/RotatingDonut.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotatingDonut : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    [SerializeField] private RectTransform rectTransform;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1 , 1), velocity * Time.deltaTime);
+    }
+}
