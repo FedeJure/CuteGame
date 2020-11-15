48ef8f05b9d1c3485c9918e8434e916a5daefd6a
Federico Jure <fjure@atixlabs.com>
2020-11-15 19:44:48 -0300

    tween on humor bar

--- Assets/Modules/Actor/Scripts/UnityDelivery/UnityHumorBarView.cs
+++ Assets/Modules/Actor/Scripts/UnityDelivery/UnityHumorBarView.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using Modules.Actor.Scripts.Presentation;
 using TMPro;
-using UniRx;
-using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,7 +13,6 @@ namespace Modules.Actor.Scripts.UnityDelivery
         public event Action OnViewDisable = () => { };
 
         [SerializeField] private Slider bar;
-        [SerializeField] private Animator animator;
         [SerializeField] private TextMeshProUGUI text;
         [SerializeField] private Color incrementColor;
         [SerializeField] private Color decrementColor;
@@ -23,22 +20,24 @@ namespace Modules.Actor.Scripts.UnityDelivery
         List<IDisposable> disposer = new List<IDisposable>();
         
 
-        private int valueChangeKey = Animator.StringToHash("valueChange");
+        private Vector3 textPosition;
+        private Vector3 textScale;
 
         private void Awake()
         {
             ActorModuleProvider.ProvidePresenterFor(this);
         }
 
+        private void Start()
+        {
+            textPosition = text.transform.position;
+            textScale = text.transform.localScale;
+        }
+
         private void OnEnable()
         {
             OnViewEnable();
             text.gameObject.SetActive(false);
-            animator.GetBehaviour<ObservableStateMachineTrigger>()
-                .OnStateMachineExitAsObservable()
-                .Do(_ => text.gameObject.SetActive(false))
-                .Subscribe()
-                .AddTo(disposer);
         }
 
         private void OnDisable()
@@ -51,10 +50,35 @@ namespace Modules.Actor.Scripts.UnityDelivery
         {
             SetValue(HumorLevel, maxHumor);
             bool increment = LastHumorChange > 0;
-            text.gameObject.SetActive(true);
+            
             text.text = increment ? $"+{LastHumorChange}" : LastHumorChange.ToString();
             text.color = increment ? incrementColor : decrementColor;
-            animator.SetTrigger(valueChangeKey);
+            LeanTween.scale(bar.gameObject, bar.transform.localScale * 1.08f, 0.2f)
+                .setLoopPingPong(1)
+                .setEaseInOutBounce();
+            TextAnimation();
+        }
+
+        private void TextAnimation()
+        {
+            var scale = text.transform.localScale;
+            var position = text.transform.position;
+            text.transform.localScale = Vector3.zero;
+            text.gameObject.SetActive(true);
+            LeanTween.scale(text.gameObject, textScale * 3f, 0.4f)
+                .setLoopPingPong(1)
+                .setOnComplete(() =>
+                {
+                    text.transform.localScale = textScale;
+                    text.gameObject.SetActive(false);
+                });
+
+            LeanTween.move(text.gameObject, textPosition + Vector3.forward / 5, 0.4f)
+                .setOnComplete(() =>
+                {
+                    text.transform.position = textPosition;
+                    text.gameObject.SetActive(false);
+                });
         }
 
         public void InitView(int humor, int maxHumor)
