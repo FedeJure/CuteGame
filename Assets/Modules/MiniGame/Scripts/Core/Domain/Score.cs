d2d8ef84b0c56d9858f840316141432321ac8ea6
Federico Jure <fedejure@gmail.com>
2022-12-03 00:02:46 -0300

    create score

---
+++ Assets/Modules/MiniGame/Scripts/Core/Domain/Score.cs
@@ -0,0 +1,13 @@
+﻿namespace Modules.MiniGame.Scripts.Core.Domain
+{
+    public struct Score
+    {
+        public float totalScore;
+        public float newScore;
+        public Score(float totalScore, float newScore)
+        {
+            this.totalScore = totalScore;
+            this.newScore = newScore;
+        }
+    }
+}
